fdc120e994aa3d453f83526e91c2cfff2d58b52a
zanderlewis <zander@zanderlewis.dev>
2025-08-02 19:05:10 -0400

    Decryptor page no longer used.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./decryptor.html",
+    "./*.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
