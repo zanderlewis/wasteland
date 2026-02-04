93cc9feeeadc95f5578426221b83af866af130df
maddavo <davek@davek.com.au>
2026-02-04 23:59:07 +1100

    Update vite.config.ts
    Updates for dual deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
   base: process.env.VITE_BASE ?? '/',
+  define: {
+    __APP_CHANNEL__: JSON.stringify(process.env.VITE_APP_CHANNEL ?? 'main')
+  },
   build: {
     outDir: 'dist',
     sourcemap: true,
