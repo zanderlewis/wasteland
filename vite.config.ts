d0ae81e569f1a4c770e755276c2b2de3683c21e3
maddavo <davek@davek.com.au>
2026-02-04 23:46:39 +1100

    Update vite.config.ts
    Updated to allow main and layout-update branches to be deployed simultaneously.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/wasteland/' : '/',
+export default defineConfig(() => ({
+  base: process.env.VITE_BASE ?? '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
@@ -18,4 +17,4 @@ export default defineConfig(({ mode }) => ({
     port: 3000,
     open: true
   }
-}))
+}));
